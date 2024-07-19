6889fa119dd7b1b92385be14e8ff8ae956cf97dc
Oskar Piotrako <oskar.piotrako@starcompliance.com>
2024-07-19 14:17:37 +0100

    Removed react icons

--- Joke-App/src/App.jsx
+++ Joke-App/src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import jokeLogo from './assets/jokegen5.png'
-import { RxEnter } from "react-icons/rx";
 
 
 const App = () => {
