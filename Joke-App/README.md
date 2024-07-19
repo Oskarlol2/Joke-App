79f48cab9183f707d809bac5bb78f8b10b67bb75
Oskar Piotrako <oskar.piotrako@starcompliance.com>
2024-07-19 11:54:39 +0100

    UI updated

--- Joke-App/README.md
+++ Joke-App/README.md
@@ -1,8 +1,2 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Joke-App is an app thats provides a random joke for you when the button "generate" is pressed!
+The app uses the official Joke API in order to create the random joke and thus credit goes to 15Dkatz and their 66 contributors!
--- Joke-App/src/App.jsx
+++ Joke-App/src/App.jsx
@@ -2,34 +2,60 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import jokeLogo from './assets/jokegen5.png'
+import { RxEnter } from "react-icons/rx";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [setup, setSetup] = useState("Setup here!");
+  const [punchline, setPunchline] = useState('Punchline here!')
+
+  const getJoke = () => {
+    const apiUrl = 'https://official-joke-api.appspot.com/random_joke';
+
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        return response.json();
+      })
+      .then(data => {
+        setSetup(data.setup)
+        setPunchline(data.punchline)
+        console.log("setup : " + data.setup);
+        console.log("punchline: " + data.punchline);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 
   return (
-    <>
+    <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={jokeLogo} className="logo" alt="joke logo"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div style={{ width: 600, display: "flex", justifyContent: "center", flexDirection: "column", paddingLeft: 50}}>
+        <div style={{ marginBottom: 10, display: "flex", flexDirection: "row" }}>
+          <div className = "App_title_wrapper">
+            Joke Generator!
+          </div>
+          <div>
+            <button onClick={() => getJoke()}>Generate</button>
+          </div>
+        </div>
+        <div className="App_joke_box">
+          {setup}
+        </div>
+        <div className="App_joke_box_2">
+          {punchline}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
+
 
 export default App
